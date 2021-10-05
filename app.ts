519a2a27233577387293176aebe6964a5e6eb8c5
AbhijithNT <abhijithnt64@gmail.com>
2021-10-05 19:53:35 +0530

    start command

--- app.ts
+++ app.ts
@@ -1,6 +1,8 @@
 import bot from "./app/bot";
+import { commands } from "./app/commands";
 
 (async () => {
+  await commands();
   await bot
     .launch()
     .then(() => console.log("Hello, World!"))
